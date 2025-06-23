abaf30e77e0c63340749183b3a47b1c65b4b507a
Alan Barragan <dalibarragan09@gmail.com>
2025-06-23 14:15:20 -0600

    Agregar la unidad de trabajo

---
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        //Aqui se agregan los diferentes repositorios
+        ICategoriaRepository Categoria { get; }
+
+        //Agrega otros repositorios según sea necesario
+        void Save();
+    }
+}
