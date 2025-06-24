f0f2a33bbf4792284faf083c5daff4b315a0f66c
Alan Barragan <dalibarragan09@gmail.com>
2025-06-23 18:26:59 -0600

    Vista de Categorias y configuracion de como debe ir para que no arroje error

---
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categorias = _contenedorTrabajo.Categoria.GetAll();
+            return Json(new { data = categorias });
+        }
+
+        #endregion
+    }
+}
