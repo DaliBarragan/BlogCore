37ca8ac749e8e58ca03f1cadc9dddfb7de9c0e6a
Alan Barragan <dalibarragan09@gmail.com>
2025-07-29 11:52:42 -0600

    Crear archivos para sembrar datos directamente en la DB al compilar el programa

---
+++ BlogCore.AccesoDatos/Data/Inicializador/InicializadorDB.cs
@@ -0,0 +1,62 @@
+using BlogCore.Data;
+using BlogCore.Models;
+using BlogCore.Utilidades;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Inicializador
+{
+    public class InicializadorDB : IInicializadorDB
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public InicializadorDB(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public void Inicializar()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            if (_db.Roles.Any(ro => ro.Name == CNT.Administrador)) return;
+
+            //Creacion de roles
+
+            _roleManager.CreateAsync(new IdentityRole(CNT.Administrador)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(CNT.Registrado)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(CNT.Cliente)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new IdentityUser
+            {
+                UserName = "admin@mail.com",
+                Email = "admin@mail.com",
+                EmailConfirmed = true,
+                //La contraseña va despues de la llave que cierra y la coma
+            }, "Admin12345*").GetAwaiter().GetResult();
+
+            IdentityUser usuario = _db.identityUsers.
+            Where(us => us.Email == "admin@mail.com").FirstOrDefault();
+            _userManager.AddToRoleAsync(usuario, CNT.Administrador).GetAwaiter().GetResult();
+
+        }
+    }
+}
